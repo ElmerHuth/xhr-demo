ff8c2224429424b2bc199ceee0429b9649fedf3e
Elmer <elmer.huth@edu.huddinge.se>
2023-10-18 11:00:07 +0200

    added data and function to populate the website with data

--- public/client.js
+++ public/client.js
@@ -6,3 +6,14 @@ document.getElementById('button').addEventListener('click', loadData);
 async function loadData() {
     let response = await fetch('https://localhost:4242/data.json');
 }
+
+function PopulateData(data) {
+    const datalist = getElementById('datalist');
+    datalist.innerHTML = '';
+
+    data.array.forEach(book => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${book.name}, ${book.year} - ${book.author}`;
+        datalist.appendChild(listItem);
+    });
+}
